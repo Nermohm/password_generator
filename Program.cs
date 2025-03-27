6551ea442d7d5306c917e9adfbf2dadcac50ca76
Nermohm <nezarmohamed112009@gmail.com>
2025-03-27 13:13:14 +0200

    Pass Gen

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+
+        static int Num()
+        {
+            
+                Random n = new Random();
+                int u = n.Next(1000, 9999);
+
+                Console.Write(u);
+                
+            
+            return u;
+        }
+        static void chart()
+        {
+            string lol = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+";
+            for (int i = 1;i < 16; i++)
+            {
+                Random n = new Random();
+                int u = n.Next(0, 65);
+                Console.Write(lol[u]);
+
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            
+            Num();
+            Console.WriteLine();
+            chart();
+        }
+    }
+}
