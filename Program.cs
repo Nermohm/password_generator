713a09fe075c3b9ad684b1ab5373a7e1fc903e96
Nermohm <111206466+Nermohm@users.noreply.github.com>
2025-05-10 14:15:45 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿namespace ConsoleApp13
+namespace ConsoleApp13
 {
     internal class Program
     {
 
         static int Num()
         {
-            
+//======================================================================Welcome To Password Generator====================================================================//
+         //Creates a passwords that contains 4 digits//  
                 Random n = new Random();
                 int u = n.Next(1000, 9999);
 
@@ -14,6 +15,7 @@
             
             return u;
         }
+        //Creates a passwords that contains 16 letters or symbols//  
         static void chart()
         {
             string lol = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+";
@@ -28,10 +30,20 @@
         }
         static void Main(string[] args)
         {
+            int x = int Parse(Console.ReadLine());
+            switch (x)
+            {
+                case 1:
+                    Num();
+                    break;
+                case 2:
+                    chart();
+                    break;
+            }
             
-            Num();
+            
             Console.WriteLine();
-            chart();
+            
         }
     }
 }
